d3760f2635bc3399f371c2d12de1edff58de8490
jordanlat <jordanlat@hotmail.fr>
2021-05-07 12:03:42 +0200

    update leg_store_apres

--- leg_store_apres.sql
+++ leg_store_apres.sql
@@ -11,11 +11,59 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de la base pour leg_store
+CREATE DATABASE IF NOT EXISTS `leg_store` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `leg_store`;
+
+-- Listage de la structure de la table leg_store. doctrine_migration_versions
+CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
+-- Listage de la structure de la table leg_store. sales
+CREATE TABLE IF NOT EXISTS `sales` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `variety_id` int(11) DEFAULT NULL,
+  `dateadd` date DEFAULT NULL,
+  `weight` int(11) DEFAULT NULL,
+  `unitprice` decimal(5,2) DEFAULT NULL,
+  `active` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_6B81704478C2BC47` (`variety_id`),
+  CONSTRAINT `FK_6B81704478C2BC47` FOREIGN KEY (`variety_id`) REFERENCES `varieties` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de la table leg_store. varieties
+CREATE TABLE IF NOT EXISTS `varieties` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vegetable_id` int(11) DEFAULT NULL,
+  `variety` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `color1` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `color2` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `color3` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_EF211793D33F4D6` (`vegetable_id`),
+  CONSTRAINT `FK_EF211793D33F4D6` FOREIGN KEY (`vegetable_id`) REFERENCES `vegetables` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de la table leg_store. vegetables
+CREATE TABLE IF NOT EXISTS `vegetables` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lifetime` int(11) DEFAULT NULL,
+  `fresh` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
